79f9043bb5002e01ee427a50ebab2d415d4bdfec
jamesbeedy <jamesbeedy@gmail.com>
2020-10-30 00:05:28 +0000

    github actions

--- scripts/push_snap.sh
+++ scripts/push_snap.sh
@@ -7,4 +7,4 @@ snap_name="flexlm-simulator"
 
 s3_loc="s3://omnivector-public-assets/snaps/$snap_name/$stage/"
 echo "Copying snap to $s3_loc"
-aws s3 cp --acl public-read ./flexlm-simulator_*_amd64.snap $s3_loc
+aws s3 cp --acl public-read flexlm-simulator_0.1_amd64.snap $s3_loc
