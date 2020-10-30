0ed2d9beecefaf7c15df73a340a6df9cd029b11e
jamesbeedy <jamesbeedy@gmail.com>
2020-10-30 01:42:53 +0000

    flexlm-simulator -> license-manager-simulator

--- scripts/push_snap.sh
+++ scripts/push_snap.sh
@@ -3,8 +3,8 @@
 set -e
 
 stage=$1
-snap_name="flexlm-simulator"
+snap_name="license-manager-simulator"
 
 s3_loc="s3://omnivector-public-assets/snaps/$snap_name/$stage/"
 echo "Copying snap to $s3_loc"
-aws s3 cp --acl public-read flexlm-simulator_0.1_amd64.snap $s3_loc
+aws s3 cp --acl public-read $snap_name_0.1_amd64.snap $s3_loc
