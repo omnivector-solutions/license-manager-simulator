c83a4a940e3af4a8022cf6a5b73bfc8865ac1f5e
zsinx6 <zsinx6@users.noreply.github.com>
2021-08-27 06:37:02 -0300

    Ensure constraints in models

--- license_manager_simulator/models.py
+++ license_manager_simulator/models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy import Column, ForeignKey, Integer, String, UniqueConstraint
 from sqlalchemy.orm import relationship
 
 from license_manager_simulator.database import Base
@@ -8,8 +8,8 @@ class License(Base):
     __tablename__ = "licenses"
 
     id = Column(Integer, primary_key=True, index=True)
-    name = Column(String, index=True)
-    total = Column(Integer)
+    name = Column(String, index=True, unique=True, nullable=False)
+    total = Column(Integer, nullable=False)
     in_use = Column(Integer, default=0)
 
     licenses_in_use = relationship("LicenseInUse", back_populates="license")
@@ -19,9 +19,13 @@ class LicenseInUse(Base):
     __tablename__ = "licenses_in_use"
 
     id = Column(Integer, primary_key=True, index=True)
-    quantity = Column(Integer)
-    user_name = Column(String)
-    lead_host = Column(String)
-    license_name = Column(String, ForeignKey("licenses.name"))
+    quantity = Column(Integer, nullable=False)
+    user_name = Column(String, nullable=False)
+    lead_host = Column(String, nullable=False)
+    license_name = Column(String, ForeignKey("licenses.name"), nullable=False)
 
     license = relationship("License", back_populates="licenses_in_use")
+
+    __table_args__ = (
+        UniqueConstraint("quantity", "user_name", "lead_host", "license_name", name="_unique_contraint"),
+    )
