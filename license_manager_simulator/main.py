dfb0c331bc3b977dae38725c47a8ea04ebb7d636
zsinx6 <zsinx6@users.noreply.github.com>
2021-08-31 16:17:08 -0300

    Add missing endpoints

--- license_manager_simulator/main.py
+++ license_manager_simulator/main.py
@@ -1,4 +1,4 @@
-from fastapi import Depends, FastAPI, HTTPException, status
+from fastapi import Body, Depends, FastAPI, HTTPException, status
 from sqlalchemy.exc import IntegrityError
 from sqlalchemy.orm import Session
 
@@ -29,3 +29,63 @@ def create_license(license: schemas.LicenseCreate, db: Session = Depends(get_db)
     except IntegrityError as e:
         raise HTTPException(status_code=status.HTTP_409_CONFLICT, detail=f"License already exists\n{e}")
     return created_license
+
+
+@app.get(
+    "/licenses/",
+    status_code=status.HTTP_200_OK,
+    response_model=list[schemas.LicenseRow],
+)
+def list_licenses(db: Session = Depends(get_db)):
+    return crud.get_licenses(db)
+
+
+@app.post(
+    "/licenses-in-use/",
+    status_code=status.HTTP_201_CREATED,
+    response_model=schemas.LicenseInUseRow,
+)
+def create_license_in_use(license_in_use: schemas.LicenseInUseCreate, db: Session = Depends(get_db)):
+    try:
+        created_license_in_use = crud.create_license_in_use(db, license_in_use)
+    except crud.NotEnoughLicenses:
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Not enough licenses available.")
+    except IntegrityError as e:
+        raise HTTPException(status_code=status.HTTP_409_CONFLICT, detail=f"LicenseInUse already exists\n{e}")
+    return created_license_in_use
+
+
+@app.get(
+    "/licenses-in-use/",
+    status_code=status.HTTP_200_OK,
+    response_model=list[schemas.LicenseInUseRow],
+)
+def list_licenses_in_use(db: Session = Depends(get_db)):
+    return crud.get_licenses_in_use(db)
+
+
+@app.get(
+    "/licenses-in-use/{license_name}",
+    status_code=status.HTTP_200_OK,
+    response_model=list[schemas.LicenseInUseRow],
+)
+def list_licenses_in_use_from_name(license_name: str, db: Session = Depends(get_db)):
+    return crud.get_licenses_in_use_from_name(db, license_name)
+
+
+@app.delete(
+    "/licenses-in-use/",
+    status_code=status.HTTP_200_OK,
+    response_model=list[int],
+)
+def delete_license_in_use(
+    lead_host: str = Body(...),
+    user_name: str = Body(...),
+    quantity: int = Body(...),
+    license_name: str = Body(...),
+    db: Session = Depends(get_db),
+):
+    try:
+        return crud.delete_license_in_use(db, lead_host, user_name, quantity, license_name)
+    except crud.LicenseNotFound:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="License not found.")
