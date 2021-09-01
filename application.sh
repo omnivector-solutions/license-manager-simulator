422efa27a4e17d8a4f9078aa25b68538fc3914b8
Heitor Pascoal de Bittencourt <heitorpbittencourt@gmail.com>
2021-09-01 14:33:31 -0300

    Fake SLURM job
    Application script generates a random user_name to credit/debit
    licenses. The number of licenses is chosen either from the CLI as the
    first argument, or hardcoded as 42.
    
    Signed-off-by: zsinx6 <zsinx6@users.noreply.github.com>

--- application.sh
+++ application.sh
@@ -1,33 +1,40 @@
 #!/usr/bin/env bash
 set -e
+
 URL="http://localhost:8000"
 
-# number of licenses
-X=1
-
-# request X licenses
-status=$(curl -s -o /dev/null -w '%{http_code}' \
-              -X 'POST' \
-              -H 'Content-Type: application/json' \
-              -d "{\"quantity\": $X,
-                  \"user_name\": \"string\",
-                  \"lead_host\": \"string\",
-                  \"license_name\": \"string\"}" \
-              "$URL"/licenses-in-use/)
-if [ "$status" = "201" ]; then
-        # there are licenses available, lets run the job
-        sleep 200
+# number of licenses: from the CLI or 42
+if [ -z $1 ]; then
+	X=42
 else
-        # not enough licenses, let's crash the job
-        tail /NOT_ENOUGH_LICENSES
+	X=$1
 fi
 
-# put the licenses back
-curl  -s -o /dev/null \
-      -X 'DELETE' \
-      -H 'Content-Type: application/json' \
-      -d "{\"quantity\": $X,
-           \"user_name\": \"string\",
-           \"lead_host\": \"string\",
-           \"license_name\": \"string\"}" \
-      "$URL"/licenses-in-use/
+dude=$(echo "user$RANDOM")
+payload="{\"quantity\": $X,
+	  \"user_name\": \"$dude\",
+	  \"lead_host\": \"string\",
+	  \"license_name\": \"string\"}"
+
+echo "Requesting $X licenses for user $dude"
+status=$(curl -s -o /dev/null -w '%{http_code}' \
+	      -X POST \
+	      -H 'Content-Type: application/json' \
+	      -d "$PAYLOAD" \
+	      "$URL"/licenses-in-use/)
+if [ "$status" = "201" ]; then
+	echo "There are enought licenses available, lets run (sleep) the job"
+	sleep 123
+else
+	echo "There are not enough licenses, let's crash the job"
+	tail /NOT_ENOUGH_LICENSES
+fi
+
+echo "Puting the licenses back"
+curl -s -o /dev/null \
+     -X DELETE \
+     -H 'Content-Type: application/json' \
+     -d "$PAYLOAD" \
+     "$URL"/licenses-in-use
+
+echo "Job done"
