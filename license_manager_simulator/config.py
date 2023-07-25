aac42a98c9d004f0bb1779e5476e3a2f6812a162
Juliana Karoline <julianaklulo@gmail.com>
2023-07-25 13:55:00 -0300

    Remove check_same_thread flag

--- license_manager_simulator/config.py
+++ license_manager_simulator/config.py
@@ -4,7 +4,7 @@ _DB_RX = r"^(sqlite|postgresql)://.+$"
 
 
 class Settings(BaseSettings):
-    DATABASE_URL: str = Field("sqlite:///./sqlite.db?check_same_thread=true", regex=_DB_RX)
+    DATABASE_URL: str = Field("sqlite:///./sqlite.db?check_same_thread=false", regex=_DB_RX)
 
     class Config:
         env_file = ".env"
